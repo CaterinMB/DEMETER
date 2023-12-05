55999337d03c0394f0e523afb8a644aa912501a9
CaterinMB <127351464+CaterinMB@users.noreply.github.com>
2023-12-05 18:58:32 -0500

    Update Estefa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+import ProtectedRoute from './ProtectedRoute.jsx'
 
 //Context
 import { Role } from './Context/Role.context.jsx'
 import { User } from './Context/User.context.jsx'
 import { Supplier } from './Context/Supplier.context.jsx'
+import { ShoppingProvider } from './Context/Shopping.context.jsx'
 import { Supplies } from './Context/Supplies.context.jsx'
 import { CategorySupplies } from './Context/CategorySupplies.context.jsx'
 import { CategoryProducts } from './Context/CategoryProducts.context.jsx'
@@ -17,6 +19,11 @@ import SuppliesPage from './Pages/SuppliesPage.jsx'
 import SuppliesCategoryPage from './Pages/SuppliesCategoryPage.jsx'
 import ProductCategoryPage from './Pages/ProductCategoryPage.jsx'
 import WaiterPage from './Pages/WaiterPage.jsx'
+import ShoppingPage from './Pages/ShoppingPage.jsx'
+import Login from './Pages/Login.jsx'
+import ResetPassword from './Pages/ResetPassword.jsx'
+import NewPassword from './Pages/NewPassword.jsx'
+import NewPurchase from './Pages/newPurchase.jsx'
 
 // Menu & Header
 import Navbar from './Components/Navbar.jsx'
@@ -32,21 +39,30 @@ function App() {
             <CategoryProducts>
               <Supplier>
                 <Supplies>
-                  <Navbar />
-                  <Header />
-                  <Routes>
-                    <Route path='/' element={<h3>DashBoard</h3>} />
-                    <Route path='/setting' element={<RolePage />} />
-                    <Route path='/user' element={<UserPage />} />
-                    <Route path='/category_supplies' element={<SuppliesCategoryPage />} />
-                    <Route path='/supplies' element={<SuppliesPage />} />
-                    <Route path='/supplier' element={<SupplierPage />} />
-                    <Route path='/shopping' element={<h3>Compras</h3>} />
-                    <Route path='/category_product' element={<ProductCategoryPage />} />
-                    <Route path='/product' element={<h3>Producto</h3>} />
-                    <Route path='/waiter' element={<WaiterPage />} />
-                    <Route path='/sale' element={<h3>Venta</h3>} />
-                  </Routes>
+                  <ShoppingProvider>
+                    <Navbar />
+                    <Header />
+                    <Routes>
+                      <Route path='/' element={<Login />} />
+                      <Route path='/resetPassword' element={<ResetPassword />} />
+                      <Route path='/newPassword/:idUser' element={<NewPassword />} />
+
+                      <Route element={<ProtectedRoute />}>
+                        <Route path='/dashboard' element={<h3>DashBoard</h3>} />
+                        <Route path='/setting' element={<RolePage />} />
+                        <Route path='/user' element={<UserPage />} />
+                        <Route path='/category_supplies' element={<SuppliesCategoryPage />} />
+                        <Route path='/supplies' element={<SuppliesPage />} />
+                        <Route path='/supplier' element={<SupplierPage />} />
+                        <Route path='/shopping' element={<ShoppingPage/>} />
+                        <Route path='/shop' element={<NewPurchase />} />
+                        <Route path='/category_product' element={<ProductCategoryPage />} />
+                        <Route path='/product' element={<h3>Producto</h3>} />
+                        <Route path='/waiter' element={<WaiterPage />} />
+                        <Route path='/sale' element={<h3>Venta</h3>} />
+                      </Route>
+                    </Routes>
+                  </ShoppingProvider>
                 </Supplies>
               </Supplier>
             </CategoryProducts>
